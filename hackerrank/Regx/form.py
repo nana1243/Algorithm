b99b3e79d29d150df3b865e6f55460ad4faa62b9
nana1243 <hieun1243@gmail.com>
2020-08-10 21:29:22 +0900

    delete dirs

--- hackerrank/Regx/form.py
+++
@@ -1,8 +0,0 @@
-#############aplication form#####################
-
-if __name__ == '__main__':
-    line_count = int(input())
-    lines = [input() for _ in range(line_count)]
-    word_count = int(input())
-    words = (input() for _ in range(word_count))
-
